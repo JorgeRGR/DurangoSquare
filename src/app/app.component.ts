810ec5476e77c4a6d8cb5c5b33acdf00676093e9
JorgeRGR <jorgerenemg@icloud.com>
2019-12-21 01:04:39 -0600

    Uso de ngFor, nfIf, ngSwitch, estilos, clases y directivas creadas por mi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DurangoSquare';
-  a = 2;
-  b = 3;
-  listo = false;
-  nombre:string = '';
-  apellido:string = '';
+  //any sirve para aceptar cualquier tipo de objeto
+  lugares:any = [
+    {plan: 'pagado', cercania: 1, distancia: 2, active: true, nombre: 'Florería Gardenia'},
+    {plan: 'gratuito', cercania: 2, distancia: 13.2, active: true, nombre: 'Donas la Jessy'},
+    {plan: 'gratuito', cercania: 3, distancia: 120, active: true, nombre: 'Veterinaria el Gato Azul'},
+    {plan: 'pagado', cercania: 1, distancia: 1.3, active: false, nombre: 'Sushi sushingada madre'},
+    {plan: 'pagado', cercania: 2, distancia: 10, active: true, nombre: 'Hotel Gobernador'},
+    {plan: 'gratuito', cercania: 3, distancia: 50, active: false, nombre: 'Liverpool'},
+  ];
+
+  personas:any = [
+    {edad: 21, nombre: 'Jorge'},
+    {edad: 23, nombre: 'Jessy'},
+    {edad: 17, nombre: 'Gabriel'},
+    {edad: 18, nombre: 'Mario'},
+    {edad: 20, nombre: 'Fernando'},
+    {edad: 16, nombre: 'David'},
+  ];
+
+  lat:number = 24.0470502;
+  lng:number = -104.6560745;
 
   constructor(){
-    setTimeout(()=> {
-      this.listo=true;
-    },3000)
-  }
-
-  doSomething(){
-    alert('Se hizo algo');
+    
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
+import { ResaltarDirective } from './directives/resaltar.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ResaltarDirective
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyC2b6Zt4PzNFuGfjdYkKcFMG4I1iPfkg9U'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
