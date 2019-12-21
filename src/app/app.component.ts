919f7b71d6c44b529bf3696803ca4478f1459b9d
JorgeRGR <jorgerenemg@icloud.com>
2019-12-21 00:00:59 -0600

    Ejemplos de data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DurangoSquare';
+  a = 2;
+  b = 3;
+  listo = false;
+  nombre:string = '';
+  apellido:string = '';
+
+  constructor(){
+    setTimeout(()=> {
+      this.listo=true;
+    },3000)
+  }
+
+  doSomething(){
+    alert('Se hizo algo');
+  }
+
 }
