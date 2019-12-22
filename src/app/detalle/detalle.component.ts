87ba2d5e5f92fda72b070363635d8995d14c1163
JorgeRGR <jorgerenemg@icloud.com>
2019-12-21 22:06:50 -0600

    Se agregó más módulos, routes e información de cada negocio en detalle

--- src/app/detalle/detalle.component.ts
+++ src/app/detalle/detalle.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-detalle',
@@ -6,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class DetalleComponent {
 
+  lugares:any = [
+    {id: 1, plan: 'pagado', cercania: 1, distancia: 2, active: true, nombre: 'Florería Gardenia', description: 'Descripción del negocio.'},
+    {id: 2, plan: 'gratuito', cercania: 2, distancia: 13.2, active: true, nombre: 'Donas la Jessy', description: 'Descripción del negocio.'},
+    {id: 3, plan: 'gratuito', cercania: 3, distancia: 120, active: true, nombre: 'Veterinaria el Gato Azul', description: 'Descripción del negocio.'},
+    {id: 4, plan: 'pagado', cercania: 1, distancia: 1.3, active: false, nombre: 'Sushi sushingada madre', description: 'Descripción del negocio.'},
+    {id: 5, plan: 'pagado', cercania: 2, distancia: 10, active: true, nombre: 'Hotel Gobernador', description: 'Descripción del negocio.'},
+    {id: 6, plan: 'gratuito', cercania: 3, distancia: 50, active: false, nombre: 'Liverpool', description: 'Descripción del negocio.'},
+  ];
+  id = null;
+  lugar:any = {};
+
+  buscarLugar(){
+    return this.lugares.filter((lugar) => {
+      return lugar.id == this.id
+    })[0] || null;
+  }
+  
+  constructor(private route:ActivatedRoute){
+    //console.log(this.route.snapshot.params['id']);
+    //console.log(this.route.queryParams);
+    //console.log(this.route.snapshot.queryParams['action']);
+    this.id = this.route.snapshot.params['id'];
+    //console.log(this.buscarLugar())
+    this.lugar = this.buscarLugar();
+  }
 }
--- src/app/lugares/lugares.component.ts
+++ src/app/lugares/lugares.component.ts
@@ -8,12 +8,12 @@ export class LugaresComponent {
   title = 'DurangoSquare';
   //any sirve para aceptar cualquier tipo de objeto
   lugares:any = [
-    {plan: 'pagado', cercania: 1, distancia: 2, active: true, nombre: 'Florería Gardenia'},
-    {plan: 'gratuito', cercania: 2, distancia: 13.2, active: true, nombre: 'Donas la Jessy'},
-    {plan: 'gratuito', cercania: 3, distancia: 120, active: true, nombre: 'Veterinaria el Gato Azul'},
-    {plan: 'pagado', cercania: 1, distancia: 1.3, active: false, nombre: 'Sushi sushingada madre'},
-    {plan: 'pagado', cercania: 2, distancia: 10, active: true, nombre: 'Hotel Gobernador'},
-    {plan: 'gratuito', cercania: 3, distancia: 50, active: false, nombre: 'Liverpool'},
+    {id: 1, plan: 'pagado', cercania: 1, distancia: 2, active: true, nombre: 'Florería Gardenia'},
+    {id: 2, plan: 'gratuito', cercania: 2, distancia: 13.2, active: true, nombre: 'Donas la Jessy'},
+    {id: 3, plan: 'gratuito', cercania: 3, distancia: 120, active: true, nombre: 'Veterinaria el Gato Azul'},
+    {id: 4, plan: 'pagado', cercania: 1, distancia: 1.3, active: false, nombre: 'Sushi sushingada madre'},
+    {id: 5, plan: 'pagado', cercania: 2, distancia: 10, active: true, nombre: 'Hotel Gobernador'},
+    {id: 6, plan: 'gratuito', cercania: 3, distancia: 50, active: false, nombre: 'Liverpool'},
   ];
 
   lat:number = 24.0470502;
